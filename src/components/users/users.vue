151b67716f4d33f5bf75dddddf6bd3091507645b
晓7777 <1726059231@qq.com>
2022-02-22 16:36:32 +0800

    用户表格

---
+++ src/components/users/users.vue
@@ -0,0 +1,67 @@
+<template>
+<el-card class="box-card">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+
+        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索 -->
+    <el-row class="searchRow">
+        <el-col>
+            <el-input placeholder="请输入内容" v-model="query" class="inputSearch">
+
+                <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+            <el-button type="success">添加用户</el-button>
+        </el-col>
+    </el-row>
+    <!-- 表格 -->
+    <el-table
+    :data="tableData"
+    style="width: 100%">
+    <el-table-column prop="date" label="日期" width="180">
+    </el-table-column>
+    <el-table-column prop="name" label="姓名" width="180">
+    </el-table-column>
+    <el-table-column prop="address" label="地址">
+    </el-table-column>
+    </el-table>
+
+</el-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            query: '',
+            tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }]
+            
+        }
+    }
+}
+</script>
+
+<style>
+.box-card {
+    height: 100%;
+}
+
+.inputSearch {
+    width: 300px;
+}
+
+.searchRow {
+    margin-top: 20px;
+}
+</style>
